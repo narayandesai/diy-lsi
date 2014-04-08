122541271bf42d9fdb6f31d440b105fbcc441e79
Narayan Desai <narayan.desai@gmail.com>
2014-04-08 16:37:12 -0500

    switch to re.finditer for output parsing. add help message

--- diylsi/inventory.py
+++ diylsi/inventory.py
@@ -15,34 +15,37 @@ def join_dict(a, b, field):
             new[item[field]] = item
     return new.values() + unmatched
 
+mptre = '\s+devhdl (?P<target>[0-9a-f]+), sasaddress (?P<guid>[0-9a-f]+),'
+
 def parse_mdb_mpt():
     ret = list()
-    for target in [item.replace(',', '').split() for item in os.popen('echo ::mptsas -t | mdb -k | grep devhdl').readlines()]:
-        ret.append({'guid':target[3], 'sas_target': int(target[1], 16)})
+    data = os.popen('echo ::mptsas -t | mdb -k').read()
+    for dev in re.finditer(mptre, data):
+        devinfo = dev.groupdict()
+        devinfo['target'] = int(devinfo['target'], 16)
+        ret.append(devinfo)
     return ret
+
+diskre = '''Device is a Hard disk
+\s+Enclosure #                             : (?P<enclosure>\d+)
+\s+Slot #                                  : (?P<slot>\d+)
+\s+SAS Address                             : (?P<sas_address>[0-9a-f\-]+)
+\s+State                                   : (?P<state>[A-Za-z \(\)]+)
+\s+Size \(in MB\)/\(in sectors\) +: (?P<size>\S+)
+\s+Manufacturer +: (?P<manufacturer>[^\n]+)
+\s+Model Number +: (?P<model>[^\n]+)
+\s+Firmware Revision +: (?P<firmware>[^\n]+)
+\s+Serial No +: (?P<serial>\S+)
+\s+GUID +: (?P<guid>\S+)
+\s+Protocol +: (?P<protocol>\S+)
+\s+Drive Type +: (?P<drivetype>\S+)
+'''
 
 def probe_lsi_controller(ctrl):
     ret = list()
-    data = [x for x in os.popen('''sas2ircu %s display | grep -A12 "Device is a Hard disk" | grep -v "is a Hard disk"''' % (ctrl)).readlines() if x != '--\n']
-    num_disks = len(data)/12
-    for disk in range(num_disks):
-        diskinfo = {'controller': ctrl}
-        base_idx = disk*12
-        disk_data = [item.split() for item in data[base_idx:base_idx+12]]
-        for entry in disk_data:
-            if entry[0] == 'Enclosure':
-                diskinfo['enclosure'] = int(entry[3])
-            elif entry[0] == 'Slot':
-                diskinfo['slot'] = int(entry[3])
-            elif entry[0:2] == ['SAS', 'Address']:
-                diskinfo['sas_address'] = entry[3]
-            elif entry[0:2] == ['Serial', 'No']:
-                diskinfo['serial'] = entry[3][:15]
-            elif entry[0] == 'GUID':
-                diskinfo['guid'] = entry[2]
-            else:
-                pass
-        ret.append( diskinfo)
+    data = os.popen('''sas2ircu %d display''' % ctrl).read()
+    for disk in re.finditer(diskre, data):
+        ret.append(disk.groupdict())
     return ret
 
 def parse_sas2ircu():

1f6fcf9987f42788fea52e36330107dc080baa92
Narayan Desai <narayan.desai@gmail.com>
2014-04-09 15:44:40 -0500

    implement led support

--- diylsi/query.py
+++ diylsi/query.py
@@ -1,23 +1,56 @@
 import argparse, json, os, sys
 
-def do_query():
-    parser = argparse.ArgumentParser(prog='diy-lsi query', description='query diy-lsi db')
-    parser.add_argument('-a', dest='all', help='print info for all', action='store_true')
-    parser.add_argument('constraints', nargs='*', help='query constraints (key=value)')
-    config = parser.parse_args(sys.argv[2:])
-
-    hwdata = json.load(open('/etc/diy-lsi/hardware.js'))
-
+def run_query(path, constraints):
+    hwdata = json.load(open(path))
     filters = {}
-    if not config.all:
+    for key, val in constraints:
+        filters[key] = val
+    return [item for item in hwdata if False not in [item.get(key, None) == value for key, value in filters.iteritems()]]
+
+class Matcher():
+    def __init__(self, name, description, callback):
+        self.parser = argparse.ArgumentParser(prog='diy-lsi %s' % name, description=description)
+        self.parser.add_argument('constraints', nargs='*', help='query constraints (key=value)')        
+        self.callback = callback
+
+    def run(self):
+        config = self.parser.parse_args(sys.argv[2:])
+        constraints = list()
         for constraint in config.constraints:
             key, value = constraint.split('=')
             try:
                 value = int(value)
             except:
                 pass
-            filters[key] = value 
+            constraints.append((key, value))
 
-    disks = [disk for disk in hwdata if False not in [disk.get(key, None) == value for key, value in filters.iteritems()]]
-    for disk in disks:
-        print disk
+        for item in run_query('/etc/diy-lsi/hardware.js', constraints):
+            self.callback(config, item)
+  
+def print_entry(_, entry):
+    print entry
+
+def do_query():
+    m = Matcher('query', 'query cache db', print_entry)
+    m.run()
+
+class LEDMatcher(Matcher):
+    def __init__(self, name, description):
+        Matcher.__init__(self, name, description, self.light)
+        self.parser.add_argument('-q', dest='q', help='query led state', action='store_true')
+        self.parser.add_argument('-0', dest='off', help='disable drive leds', action='store_true')
+        self.parser.add_argument('-1', dest='on', help='light drive leds', action='store_true')
+
+    def light(self, config, entry):
+        if len([x for x in entry.keys() if x in ['ctrl', 'slot', 'enclosure']]) != 3:
+            print "Malformed entry :%s: for id" % entry
+            return
+        if config.on:
+            mode = 'on'
+        elif config.off:
+            mode = 'off'
+        os.popen("sas2ircu %s LOCATE %d:%d %s" % (entry['ctrl'], entry['enclosure'], entry['slot'], mode)).read()
+
+def do_id():
+    L = LEDMatcher('id', 'set LED state')
+    L.run()
